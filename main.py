2d012ec96fbdeffc2b319077a799cde039095849
DaveEllio <117090716+DaveEllio@users.noreply.github.com>
2022-11-04 11:25:27 -0500

    npc symbol change

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main() -> None:
     event_handler = EventHandler()
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "¥", (255, 255, 0))
     entities = {npc, player}
 
     engine = Engine(entities=entities, event_handler=event_handler, player=player)
